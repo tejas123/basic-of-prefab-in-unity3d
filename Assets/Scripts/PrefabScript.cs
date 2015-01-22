5dd0ad96c22509d4230bcd2b9d93c88217981329
Bharat <bharatappguru@gmail.com>
2015-01-22 13:31:39 +0530

    Basics of Prefab in Unity3D

---
+++ Assets/Scripts/PrefabScript.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PrefabScript : MonoBehaviour {
+    /// <summary>
+    /// Whenever user click on prefab, it will change color of it.
+    /// </summary>
+    void OnMouseDown()
+    {
+        this.renderer.material.color = Color.green;
+    }
+}
---
+++ Assets/Scripts/SphereCreator.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SphereCreator : MonoBehaviour {
+    
+    //Refrence of Prefab.
+    public GameObject ball;
+    
+    int xPosition=0, yPosition=3;
+   
+    //This will call whenever user click on plane.
+    void OnMouseDown()
+    {
+        Instantiate(ball, new Vector3(xPosition,yPosition,0),Quaternion.identity);
+        xPosition += 1;
+    }
+
+}
+    
